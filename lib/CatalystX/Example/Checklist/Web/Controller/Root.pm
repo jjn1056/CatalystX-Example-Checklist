790df58a97d17a7fe0b9d68cb194a5151030c991
John Napiorkowski <jjnapiork@cpan.org>
2013-01-04 21:51:32 -0500

    basic checklist

--- lib/CatalystX/Example/Checklist/Web/Controller/Root.pm
+++ lib/CatalystX/Example/Checklist/Web/Controller/Root.pm
@@ -15,16 +15,29 @@ sub start : Chained('/')
     $ctx->stash->{items} = $ctx->session->{items};
   }
 
-  sub validate_checklist: Chained('start')
+  sub add : Chained('start')
    PathPart('') Args(0) Method('POST')
   {
     my ($self, $ctx) = @_;
     my $post = $ctx->req->body_parameters;
     push @{$ctx->session->{items}}, $post->{'new-item'};
+    $ctx->go('my_first_checklist');
   }
 
-  sub default: Chained('start')
+  sub default : Chained('start')
     PathPart('') Args(0) { pop->res->body('NOT ALLOWED') }
+
+  sub delete : Chained('start')
+    PathPart('delete') Args(0) Method('POST')
+  {
+    my ($self, $ctx) = @_;
+    my $post = $ctx->req->body_parameters;
+    splice @{$ctx->session->{items}}, $post->{'delete-item'}, 1;
+    $ctx->res->redirect(
+      $ctx->uri_for_action(
+        $ctx->controller->action_for('my_first_checklist')));
+    $ctx->detach;
+  }
 
 
 sub end : ActionClass('RenderView') { }
